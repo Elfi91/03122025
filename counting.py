7162ce2846a4dea686dbf53e09c8f0c1f4f35856
Elisabetta Fino <elisabetta.fino@gmail.com>
2025-12-04 09:24:20 +0100

    Translate counting.py

--- counting.py
+++ counting.py
@@ -1,22 +1,22 @@
 '''
-Obiettivo: Usare dizionari per contare occorrenze.
+Goal: Use dictionaries to count occurrences.
 '''
 
-# Creare una lista voti contenente: ["A", "B", "A", "C", "B", "A", "D", "B", "C", "A"]
-lista_voti: list[str] = ["A", "B", "A", "C", "B", "A", "D", "B", "C", "A"]
+# Create a 'grades' list containing: ["A", "B", "A", "C", "B", "A", "D", "B", "C", "A"]
+grade_list: list[str] = ["A", "B", "A", "C", "B", "A", "D", "B", "C", "A"]
 
-# Creare un dizionario vuoto conteggio
-conteggio: dict[str, int] = {}
+# Create an empty 'count' dictionary
+count: dict[str, int] = {}
 
-# Iterare sulla lista voti e contare quante volte appare ogni voto nel dizionario
-for voto in lista_voti:
-    # 1. Verifica se il voto è già una chiave nel dizionario
-    if voto in conteggio:
-        # Se è presente, incrementa il conteggio di 1
-        conteggio[voto] += 1
+# Iterate over the 'grade_list' and count how many times each grade appears in the dictionary
+for grade in grade_list:
+    # 1. Check if the grade is already a key in the dictionary
+    if grade in count:
+        # If present, increment the count by 1
+        count[grade] += 1
     else:
-        # Se non è presente, aggiungi la chiave e inizializza il conteggio a 1
-        conteggio[voto] = 1
+        # If not present, add the key and initialize the count to 1
+        count[grade] = 1
 
-# Stampare il dizionario finale
-print(conteggio)
+# Print the final dictionary
+print(count)
