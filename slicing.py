03450cf485361b260eaaf6f36f8f1df1fbc00fb6
Elisabetta Fino <elisabetta.fino@gmail.com>
2025-12-04 09:01:29 +0100

    trasnlate slicing.py

--- slicing.py
+++ slicing.py
@@ -1,27 +1,23 @@
 '''
-Obiettivo: Praticare l'accesso agli elementi e lo slicing.
+Goal: Practice element access and slicing.
 '''
 
-# Creare una lista temperature
-Lista_iniziale: list[str] = ["15", "18", "22", "25", "28", "30", "27", "24", "20"] 
-
-# Ordinare i numeri automaticamente
-# Lista_ordinata = sorted(Lista_iniziale) 
-
+# Create a 'temperatures' list
+Initial_list: list[str] = ["15", "18", "22", "25", "28", "30", "27", "24", "20"] 
 
 # ------------ #
 
-# Stampare la prima temperatura
-print(Lista_iniziale[0]) 
+# Print the first temperature
+print(Initial_list[0]) 
 
-# Stampare l'ultima temperatura
-print(Lista_iniziale[-1]) 
+# SPrint the last temperature
+print(Initial_list[-1]) 
 
-# Stampare le temperature dalla posizione 2 alla 5 (esclusa)
-print(Lista_iniziale[1:4])
+# Print the temperatures from position 2 up to 5 (excluse)
+print(Initial_list[1:4])
 
-# Stampare tutte le temperature con step 2 (saltando una ogni due)
-print(Lista_iniziale[::2])
+# Print all temperatures with step 2 (skipping one every two)
+print(Initial_list[::2])
 
-# Stampare la lista ordinata
-print(Lista_iniziale)
+# Print the list
+print(Initial_list)
